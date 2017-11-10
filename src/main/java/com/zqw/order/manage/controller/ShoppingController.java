6a1e062e58cf2a4649e99e6cbc54801b42422c43
zhuquanwen <12345678a>
2017-11-10 16:42:51 +0800

    shopping

--- src/main/java/com/zqw/order/manage/controller/ShoppingController.java
+++ src/main/java/com/zqw/order/manage/controller/ShoppingController.java
@@ -2,6 +2,7 @@ package com.zqw.order.manage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @author: zhuquanwen
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ShoppingController {
-    @GetMapping("/toShopping")
-    public String toShopping(){
+//    @GetMapping(value={"/toShopping"})
+//    public String toShopping(@PathVariable String userInfo){
+//        System.out.println(userInfo);
+//        return "shopping";
+//    }
+
+    @GetMapping(value={"/toShopping/{userInfo}"})
+    public String toShoppingUserInfo(@PathVariable String userInfo){
+        System.out.println(userInfo);
         return "shopping";
     }
 }
---
+++ src/main/resources/static/shopping/shopping.js
@@ -0,0 +1,119 @@
+
+$(function(){
+    $("#tijiao").click(function(){
+        var arrPerson=new Array();
+        var id      = $("#id").val();
+        var typeid  = $("#typeid").val();
+        var name  = $("#name").val();
+        var tel   = $("#phoneNum").val();
+
+        var xs    = $("#xs").val();
+        var postarea_prov     = $("#cmbProvince").val();
+        var postarea_city     = $("#cmbCity").val();
+        var postarea_country  = $("#cmbArea").val();
+        var address  = $("#address").val();
+        var tel_yz    = /^1[34578]\d{9}$/;
+        var teltype =$('input:radio[name="cardType"]:checked').val();
+        var items =$('input:radio[name="items"]:checked').val();
+        var person =  new Object();
+        var len = $(".attr").length;
+        var style = $("#style").val();
+        var clothSize = $("#clothSize").val();
+        if (name == '') {
+            alert('请输入姓名');
+            return false;
+        }
+        if (tel == '') {
+            alert('请输入手机号');
+            return false;
+        }
+        if(!tel_yz.test(tel)){
+            alert("请填写正确手机号");
+            return false;
+        }
+        if(teltype==null){
+            alert("电话卡充值类型!");
+            return false;
+        }
+        if (postarea_prov == '' || postarea_prov ==  "-1") {
+            alert('请填写省/自治区/直辖市');
+            return false;
+        }
+        if (postarea_city == '' || postarea_city == "-1") {
+            alert('请填写市/区');
+            return false;
+        }
+
+        if (postarea_country == '' || postarea_country == "-1") {
+            alert('请填写区/县');
+            return false;
+        }
+
+        if (address == '') {
+            alert('请输入详细地址');
+            return false;
+        }
+        if(style == null || style == ""){
+            alert('请选择款式');
+            return false;
+        }
+        if(clothSize == null || clothSize == ""){
+            alert('请选择尺码');
+            return false;
+        }
+
+        // if (len > 0 ) {
+        //     for (var i = 0; i < len; i++) {
+        //         var attr = $(".attr").eq(i).val();
+        //         var attid = $(".attid").eq(i).val();
+        //         person[attid] = $(".attr").eq(i).val();
+        //         if (attr == '') {
+        //             str=$('.xiug').eq(i).text();
+        //             alert('请选择'+str.substr(0,str.length-3));
+        //             return false;
+        //
+        //         }
+        //     }
+        //     arrPerson.push(person);
+        // }
+        if(items==null){
+            alert("请确认运费自理!");
+            return false;
+        }
+        $.ajax({
+            url  : 'member.php',
+            type : 'post',
+            data : {'a':'goodsorder','attr':arrPerson,'id':id,'xs':xs,'name':name,'tel':tel,'postarea_prov':postarea_prov,'postarea_city':postarea_city,'postarea_country':postarea_country,'address':address,'typeid':typeid,'teltype':teltype},
+            dataType:'html',
+            success:function(data){
+                if (data==1) {
+                    alert('订单提交成功');
+                    $("#page").hide();
+                    $("#page2").show();
+                    return false;
+
+                }else{
+                    alert('订单提交失败');
+                    return false;
+                }
+            }
+        });
+
+
+
+    })
+});
+
+/*$.ajax({
+    url  : 'member.php',
+    type : 'post',
+    data : { },
+    dataType:'html',
+    success:function(data){
+        console.log(data);
+        if (data ==1) {
+            window.history.go(-1);
+        }
+    }
+});*/
+
