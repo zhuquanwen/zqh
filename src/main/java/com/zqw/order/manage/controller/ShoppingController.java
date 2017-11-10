3a6555715fb2b036da87ee4619024fb3b97949cf
zhuquanwen <12345678a>
2017-11-10 17:01:43 +0800

    shopping

--- src/main/java/com/zqw/order/manage/controller/ShoppingController.java
+++ src/main/java/com/zqw/order/manage/controller/ShoppingController.java
@@ -3,6 +3,10 @@ package com.zqw.order.manage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: zhuquanwen
@@ -17,9 +21,13 @@ public class ShoppingController {
 //        return "shopping";
 //    }
 
-    @GetMapping(value={"/toShopping/{userInfo}"})
-    public String toShoppingUserInfo(@PathVariable String userInfo){
-        System.out.println(userInfo);
-        return "shopping";
+    @GetMapping(value={"/toShopping/{userInfo}/{id}"})
+    public ModelAndView toShoppingUserInfo(@PathVariable String userInfo, @PathVariable Integer id){
+        //通过ID获得图片信息，库存信息等。
+        ModelAndView mav = new ModelAndView("shopping");
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("title","阿迪2017明星同款套装免费送");
+        mav.getModel().put("topTitle","阿迪2017明星同款套装免费送");
+        return mav;
     }
 }
